21ffcccc958e0b73861f13b4f214d97a71242487
do.hoang.than <do.hoang.than@sun-asterisk.com>
2023-01-11 15:25:11 +0700

    adding item

--- Scripts/Base/UI/ScrollViewTemplate.cs
+++ Scripts/Base/UI/ScrollViewTemplate.cs
@@ -1,21 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 namespace PJLived.GunnerStars.FirstGame.UI.Template
 {
+    // <summary>
+    // Hierychy : 
+    // ScrollView
+    //   -> ViewPort
+    //      -> Content              // Sibling Index : Must 01
+    //         -> Item
+    //      -> Horizon  Bar         // Don't care. May delete.
+    //      -> Vertical Bar         // Don't care. May delete
+    ///     -> ContentPrefab        // Must UN-enable
+    // </summary>
     public class ScrollViewTemplate : MonoBehaviour
     {
+        #region Serialized
+        [SerializeField] GameObject itemPrefab;
+        #endregion
+
         #region Private vars
-        ScrollView scrollView;
+        ScrollRect scrollViewScript;
+        GameObject scrollView;
+        GameObject viewPort;
+        GameObject content;
+        GameObject contentPrefab;
         #endregion
 
         #region Mono
         private void Awake()
         {
-            scrollView = GetComponent<ScrollView>();
+            Initialized();
+        }
+        #endregion
 
+        #region Private methods
+        [ContextMenu("Initialized")]
+        private void Initialized()
+        {
+            scrollViewScript = GetComponent<ScrollRect>();
+            if (scrollViewScript == null)
+            {
+                Debug.LogWarning("Init error.");
+                return;
+            }
+            scrollView = scrollViewScript.gameObject;
+            viewPort = scrollViewScript.viewport.gameObject;
+            content = scrollViewScript.content.gameObject;
+            if (contentPrefab == null)
+            {
+                contentPrefab = Instantiate(content, scrollView.transform);
+                contentPrefab.SetActive(false);
+                contentPrefab.name = "ContentPrefab";
+            }
+        }
+        #endregion
+
+        #region public methods
+        /// <summary>
+        /// Delete and re-create is way faster than delete one by one.
+        /// </summary>
+        [ContextMenu("ResetScroll")]
+        public void ResetScroll()
+        {
+            if (content == null)
+            {
+                Debug.Log("Can't reset. Content null.");
+                return;
+            }
+            if (viewPort == null) return;
+            if (Application.isPlaying) Destroy(content.gameObject);
+            else DestroyImmediate(content.gameObject);
+            content = Instantiate(contentPrefab, viewPort.transform);
+            content.SetActive(true);
+            content.transform.SetAsFirstSibling();
+            content.name = "Content";
+            scrollViewScript.content = content.GetComponent<RectTransform>();
+        }
+        /// <summary>
+        /// Add a new Item.
+        /// Item should have interface.
+        /// If index = -1, add new at the end.
+        /// </summary>
+        public virtual void AddItem<T>(T data, int index = -1)
+        {
+            if (itemPrefab == null)
+            {
+                Debug.Log("Item prefab null.");
+                return;
+            }
+            if (content == null)
+            {
+                Debug.Log("Content null.");
+                return;
+            }
+            GameObject newItem = Instantiate(itemPrefab, content.transform);
+            UIItemInterface iItem = newItem.GetComponent<UIItemInterface>();
+            if (iItem != null) iItem.Setup(data);
         }
         #endregion
     }
---
+++ Scripts/Base/UI/UIItemInterface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PJLived.GunnerStars.FirstGame.UI.Template
+{
+    public interface UIItemInterface
+    {
+        void Setup<T>(T data);
+    }
+}
