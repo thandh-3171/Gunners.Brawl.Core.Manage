ed52212a3c06fdac93a6def3f8d01ed835412a39
do.hoang.than <do.hoang.than@sun-asterisk.com>
2023-02-01 13:27:31 +0700

    need again

---
+++ Scripts/Base/UI/Template/UIItemTemplate.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeAreProStars.Core.Manage.UI.Template
+{
+    public class UIItemTemplate : UIItemAbstract
+    {
+        public override void Activate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ActivateQueue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Initialized()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnClick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnPostAdded_SetupUI<T>(T data, GameObject entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnPostQueueAdded_SetupUI<T>(T data, GameObject entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEnumerator<float> _ActivateQueue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEnumerator<float> _OnPostQueueAdded_SetupUI<T>(T data, GameObject entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
