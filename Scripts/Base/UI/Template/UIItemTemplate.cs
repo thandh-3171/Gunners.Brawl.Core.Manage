390400118bcbf4a60f32f9bda9fb848a592f6d04
do.hoang.than <do.hoang.than@sun-asterisk.com>
2023-02-01 14:02:09 +0700

    remove null exception

--- Scripts/Base/UI/Template/UIItemTemplate.cs
+++ Scripts/Base/UI/Template/UIItemTemplate.cs
@@ -1,3 +1,4 @@
+using MEC;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,44 +7,13 @@ namespace WeAreProStars.Core.Manage.UI.Template
 {
     public class UIItemTemplate : UIItemAbstract
     {
-        public override void Activate()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void ActivateQueue()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void Initialized()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void OnClick()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void OnPostAdded_SetupUI<T>(T data, GameObject entity)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void OnPostQueueAdded_SetupUI<T>(T data, GameObject entity)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override IEnumerator<float> _ActivateQueue()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override IEnumerator<float> _OnPostQueueAdded_SetupUI<T>(T data, GameObject entity)
-        {
-            throw new System.NotImplementedException();
-        }
+        public override void Activate() { }
+        public override void ActivateQueue() { }
+        public override void Initialized() { }
+        public override void OnClick() { }
+        public override void OnPostAdded_SetupUI<T>(T data, GameObject entity) { }
+        public override void OnPostQueueAdded_SetupUI<T>(T data, GameObject entity) { }
+        public override IEnumerator<float> _ActivateQueue() { yield break; }
+        public override IEnumerator<float> _OnPostQueueAdded_SetupUI<T>(T data, GameObject entity) { yield break; }
     }
 }
